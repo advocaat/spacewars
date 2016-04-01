cc96b474065373ce7ebf24f6117da7fc7751994c
advocaat <adam.nicholls@my.jcu.edu.au>
2016-04-02 00:14:36 +1000

    moving pieces and win condition

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,16 @@
 var express = require('express');
+var bodyParser = require('body-parser');
 var router = express.Router();
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+router.get('/', function (req, res, next) {
+    res.render('index', {title: 'Express'});
 });
 
+router.post('/gameOver', function (req, res, next) {
+    var cuntsnatcher = JSON.stringify(req.body.time);
+    console.log("poop" + cuntsnatcher);
+    res.render('gameOver',{poof: cuntsnatcher});
+
+});
 module.exports = router;
